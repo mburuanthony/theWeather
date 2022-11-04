b73a0bf164c642f0ae89adc2e063290b6520adf2
mburuanthony <mburuantony@outlook.com>
2022-11-04 17:53:41 +0300

    modified image.jsx

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,43 +1,35 @@
 import "../Assets/Styles/Image.css";
 import ClearImage from "../Assets/Images/Clear.png";
 import HailImage from "../Assets/Images/Hail.png";
-import HeavyCloudImage from "../Assets/Images/HeavyCloud.png";
 import HeavyRainImage from "../Assets/Images/HeavyRain.png";
 import LightClodImage from "../Assets/Images/LightCloud.png";
 import LightRainImage from "../Assets/Images/LightRain.png";
 import ShowerImage from "../Assets/Images/Shower.png";
-import SleetImage from "../Assets/Images/Sleet.png";
 import SnowImage from "../Assets/Images/Snow.png";
 import ThunderImage from "../Assets/Images/Thunderstorm.png";
 
-function Image(props) {
-  const { weatherStateName } = props;
-
+function Image({ weathercode }) {
   return (
     <div className="weatherImage">
       <img
         src={
-          weatherStateName === "Heavy Cloud"
-            ? HeavyCloudImage
-            : weatherStateName === "Showers"
-            ? ShowerImage
-            : weatherStateName === "Snow"
-            ? SnowImage
-            : weatherStateName === "Sleet"
-            ? SleetImage
-            : weatherStateName === "Hail"
-            ? HailImage
-            : weatherStateName === "Thunderstorm"
-            ? ThunderImage
-            : weatherStateName === "Heavy Rain"
-            ? HeavyRainImage
-            : weatherStateName === "Light Rain"
-            ? LightRainImage
-            : weatherStateName === "Light Cloud"
+          weathercode >= 1 && weathercode <= 3
             ? LightClodImage
+            : weathercode >= 51 && weathercode <= 55
+            ? ShowerImage
+            : weathercode >= 66 && weathercode <= 67
+            ? HeavyRainImage
+            : weathercode >= 71 && weathercode <= 75
+            ? SnowImage
+            : weathercode >= 80 && weathercode <= 82
+            ? LightRainImage
+            : weathercode >= 96
+            ? HailImage
+            : weathercode === 95
+            ? ThunderImage
             : ClearImage
         }
-        alt={weatherStateName}
+        alt={weathercode}
       />
     </div>
   );
